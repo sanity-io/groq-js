ffffa02a141d2646bdc996fb142d4a55c4fd4504
Knut Melvær <knut.melvaer@gmail.com>
2019-07-10 07:24:14 +0100

    add match

--- src/evaluator/operators.js
+++ src/evaluator/operators.js
@@ -57,7 +57,7 @@ exports['<='] = function count(left, right, scope, execute) {
 exports['in'] = function count(left, right, scope, execute) {
   return new Value(async () => {
     let a = await execute(left, scope).get()
-    
+
     for await (let b of execute(right, scope)) {
       if (a == b) {
         return true
@@ -65,5 +65,15 @@ exports['in'] = function count(left, right, scope, execute) {
     }
 
     return false
+  })
+}
+
+exports ['match'] = function match(left, right, scope, execute) {
+  return new Value(async () => {
+    let a = await execute(left, scope).get()
+    let b = await execute(right, scope).get()
+    let regex = b.replace('*', '.*')
+
+    return new RegExp(regex).test(a)
   })
 }
