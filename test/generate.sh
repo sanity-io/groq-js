6ea732c58d4130069978d3437c2d09946e533ee6
Magnus Holm <judofyr@gmail.com>
2020-06-08 15:03:44 +0200

    Use updated groq-test-suite

--- test/generate.sh
+++ test/generate.sh
@@ -2,20 +2,10 @@
 set -eo pipefail
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-if [ -z "$1" ]; then
-  echo "usage: $0 <directory>"
-  echo
-  echo " (where <directory> is a clone of https://github.com/sanity-io/groq-test-suite)"
-  exit 1
-fi
-
 RESULT="$DIR"/suite.test.ts
 
-(
-  cd "$1" &&
-  yarn --silent build
-) | node "$DIR"/generate.js > "$RESULT"
+curl -sfL 'https://github.com/sanity-io/groq-test-suite/releases/latest/download/suite.ndjson' | \
+  node "$DIR"/generate.js > "$RESULT"
 
 $DIR/../node_modules/.bin/prettier --write "$RESULT"
 
