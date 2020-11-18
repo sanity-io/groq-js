edc79802d0021089785d3955d619c1fbc103022c
Magnus Holm <judofyr@gmail.com>
2020-11-18 11:27:10 +0100

    Lock groq-test-suite version

--- test/generate.sh
+++ test/generate.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
+GROQTEST_SUITE_VERSION=${GROQTEST_SUITE_VERSION:-v0.1.8}
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 RESULT="$DIR"/suite.test.ts
 
-curl -sfL 'https://github.com/sanity-io/groq-test-suite/releases/latest/download/suite.ndjson' | \
+curl -sfL "https://github.com/sanity-io/groq-test-suite/releases/download/$GROQTEST_SUITE_VERSION/suite.ndjson" | \
   node "$DIR"/generate.js > "$RESULT"
 
 $DIR/../node_modules/.bin/prettier --write "$RESULT"
