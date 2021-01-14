df31775859d180d270f1e07a9cde3364c90b6b1d
Magnus Holm <judofyr@gmail.com>
2021-01-14 13:42:19 +0100

    Update to latest groq-test-suite
    But disable dateTime test for now

--- test/generate.sh
+++ test/generate.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-GROQTEST_SUITE_VERSION=${GROQTEST_SUITE_VERSION:-v0.1.8}
+GROQTEST_SUITE_VERSION=${GROQTEST_SUITE_VERSION:-v0.1.9}
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 RESULT="$DIR"/suite.test.ts
