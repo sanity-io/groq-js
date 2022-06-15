7d71630278d231c43e2e91979c5420c01b008e96
Alexander Staubo <alex@sanity.io>
2022-06-15 14:07:59 +0200

    feat: generate.sh: support local test suite file via $GROQTEST_SUITE.

--- test/generate.sh
+++ test/generate.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-GROQTEST_SUITE_VERSION=${GROQTEST_SUITE_VERSION:-v0.1.33}
-
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 RESULT="$DIR"/suite.test.js
 
-curl -sfL "https://github.com/sanity-io/groq-test-suite/releases/download/$GROQTEST_SUITE_VERSION/suite.ndjson" |
-  node "$DIR"/generate.js >"$RESULT"
+if [[ "$GROQTEST_SUITE" != "" ]]; then
+  node "$DIR"/generate.js < "$GROQTEST_SUITE" >"$RESULT"
+else
+  GROQTEST_SUITE_VERSION=${GROQTEST_SUITE_VERSION:-v0.1.33}
+  curl -sfL "https://github.com/sanity-io/groq-test-suite/releases/download/$GROQTEST_SUITE_VERSION/suite.ndjson" |
+    node "$DIR"/generate.js >"$RESULT"
+fi
 
 $DIR/../node_modules/.bin/prettier --write "$RESULT"
 
