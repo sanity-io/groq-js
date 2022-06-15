1406269d716f53019c1feecd044103dd33d5e40e
Alexander Staubo <alex@sanity.io>
2022-06-15 13:29:45 +0200

    feat: adds string::split(), array::compact(), array::join().

--- src/evaluator/functions.ts
+++ src/evaluator/functions.ts
@@ -261,6 +261,27 @@ string.upper = async function (args, scope, execute) {
 }
 string.upper.arity = 1
 
+string.split = async function (args, scope, execute) {
+  const str = await execute(args[0], scope)
+  if (str.type !== 'string') {
+    return NULL_VALUE
+  }
+  const sep = await execute(args[1], scope)
+  if (sep.type !== 'string') {
+    return NULL_VALUE
+  }
+
+  if (str.data.length === 0) {
+    return fromJS([])
+  }
+  if (sep.data.length === 0) {
+    // This uses a Unicode codepoint splitting algorithm
+    return fromJS(Array.from(str.data))
+  }
+  return fromJS(str.data.split(sep.data))
+}
+string.split.arity = 2
+
 global.lower = string.lower
 global.upper = string.upper
 
@@ -278,6 +299,57 @@ string.startsWith = async function (args, scope, execute) {
   return str.data.startsWith(prefix.data) ? TRUE_VALUE : FALSE_VALUE
 }
 string.startsWith.arity = 2
+
+const array: FunctionSet = {}
+
+array.join = async function (args, scope, execute) {
+  const arr = await execute(args[0], scope)
+  if (!arr.isArray()) {
+    return NULL_VALUE
+  }
+  const sep = await execute(args[1], scope)
+  if (sep.type !== 'string') {
+    return NULL_VALUE
+  }
+  let buf = ''
+  let needSep = false
+  for await (const elem of arr) {
+    if (needSep) {
+      buf += sep.data
+    }
+    switch (elem.type) {
+      case 'number':
+      case 'string':
+      case 'boolean':
+      case 'datetime':
+        buf += `${elem.data}`
+        break
+      default:
+        return NULL_VALUE
+    }
+    needSep = true
+  }
+  return fromJS(buf)
+}
+array.join.arity = 2
+
+array.compact = async function (args, scope, execute) {
+  const arr = await execute(args[0], scope)
+  if (!arr.isArray()) {
+    return NULL_VALUE
+  }
+
+  const result: Array<any> = []
+  for await (const elem of arr) {
+    const val = await elem.get()
+    if (val !== null) {
+      result.push(val)
+    }
+  }
+
+  return fromJS(result)
+}
+array.compact.arity = 0
 
 const pt: FunctionSet = {}
 pt.text = async function (args, scope, execute) {
@@ -459,6 +531,7 @@ diff.changedOnly.arity = 3
 export const namespaces: NamespaceSet = {
   global,
   string,
+  array,
   pt,
   delta,
   diff,
