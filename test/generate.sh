9005a6bbc67c1367b89b94d4ef95780bf0a472f8
Alexander Staubo <alex@sanity.io>
2022-07-21 15:40:43 +0200

    tests: use GROQ test suite version 0.1.35.

--- test/generate.sh
+++ test/generate.sh
@@ -7,7 +7,7 @@ RESULT="$DIR"/suite.test.js
 if [[ "$GROQTEST_SUITE" != "" ]]; then
   node "$DIR"/generate.js < "$GROQTEST_SUITE" >"$RESULT"
 else
-  GROQTEST_SUITE_VERSION=${GROQTEST_SUITE_VERSION:-v0.1.33}
+  GROQTEST_SUITE_VERSION=${GROQTEST_SUITE_VERSION:-v0.1.35}
   curl -sfL "https://github.com/sanity-io/groq-test-suite/releases/download/$GROQTEST_SUITE_VERSION/suite.ndjson" |
     node "$DIR"/generate.js >"$RESULT"
 fi
