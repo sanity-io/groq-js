c4676415eac74cbab722b97bf82b34f4749cd1b5
Alexander Staubo <alex@sanity.io>
2022-07-21 15:59:11 +0200

    feat: implements math::*() functions.

--- src/evaluator/functions.ts
+++ src/evaluator/functions.ts
@@ -527,6 +527,88 @@ diff.changedOnly = () => {
 }
 diff.changedOnly.arity = 3
 
+const math: FunctionSet = {}
+math.min = async function (args, scope, execute) {
+  const arr = await execute(args[0], scope)
+  if (!arr.isArray()) {
+    return NULL_VALUE
+  }
+
+  let n: number | undefined = undefined
+  for await (const elem of arr) {
+    if (elem.type === 'null') continue
+    if (elem.type !== 'number') {
+      return NULL_VALUE
+    }
+    if (n === undefined || elem.data < n) {
+      n = elem.data
+    }
+  }
+  return fromJS(n)
+}
+math.min.arity = 1
+
+math.max = async function (args, scope, execute) {
+  const arr = await execute(args[0], scope)
+  if (!arr.isArray()) {
+    return NULL_VALUE
+  }
+
+  let n: number | undefined = undefined
+  for await (const elem of arr) {
+    if (elem.type === 'null') continue
+    if (elem.type !== 'number') {
+      return NULL_VALUE
+    }
+    if (n === undefined || elem.data > n) {
+      n = elem.data
+    }
+  }
+  return fromJS(n)
+}
+math.max.arity = 1
+
+math.sum = async function (args, scope, execute) {
+  const arr = await execute(args[0], scope)
+  if (!arr.isArray()) {
+    return NULL_VALUE
+  }
+
+  let n = 0
+  for await (const elem of arr) {
+    if (elem.type === 'null') continue
+    if (elem.type !== 'number') {
+      return NULL_VALUE
+    }
+    n += elem.data
+  }
+  return fromJS(n)
+}
+math.sum.arity = 1
+
+math.avg = async function (args, scope, execute) {
+  const arr = await execute(args[0], scope)
+  if (!arr.isArray()) {
+    return NULL_VALUE
+  }
+
+  let n = 0
+  let c = 0
+  for await (const elem of arr) {
+    if (elem.type === 'null') continue
+    if (elem.type !== 'number') {
+      return NULL_VALUE
+    }
+    n += elem.data
+    c++
+  }
+  if (c === 0) {
+    return NULL_VALUE
+  }
+  return fromJS(n / c)
+}
+math.avg.arity = 1
+
 export const namespaces: NamespaceSet = {
   global,
   string,
@@ -535,4 +617,5 @@ export const namespaces: NamespaceSet = {
   delta,
   diff,
   sanity,
+  math,
 }
