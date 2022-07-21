657043677d6381cf12fea962f215d48e2add69fc
Matt Craig <matt@sanity.io>
2022-07-21 12:54:53 +0100

    feat: add test case for MarkProcessor

---
+++ test/markProcessorTest.test.ts
@@ -0,0 +1,20 @@
+import t from 'tap'
+import * as NodeTypes from '../src/nodeTypes'
+import {MarkProcessor, MarkVisitor} from '../src/markProcessor'
+import {throwsWithMessage} from './testUtils'
+
+const TestVisitor: MarkVisitor<NodeTypes.ExprNode> = {
+  valid(p) {
+    return {type: 'Everything'}
+  },
+}
+
+t.test('MarkProcessor', async (t) => {
+  t.test('throws when an unknown handler is called', async () => {
+    const marks = [{name: 'invalid', position: 0}]
+    const input = ''
+    const processor = new MarkProcessor(input, marks, {})
+
+    throwsWithMessage(() => processor.process(TestVisitor), 'Unknown handler: invalid')
+  })
+})
