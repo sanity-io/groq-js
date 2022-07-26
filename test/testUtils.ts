9461bc0b1d7d7a34d1c66c8244a4b713a9e0e625
Matt Craig <matt@sanity.io>
2022-07-26 09:24:31 +0100

    fix: ensure test helper catches functions that don't throw, but should

--- test/testUtils.ts
+++ test/testUtils.ts
@@ -5,9 +5,14 @@ export async function throwsWithMessage(
   funcUnderTest: () => {},
   expectedMessage: string
 ) {
+  let didThrow = false
+
   try {
     await funcUnderTest()
   } catch (error: any) {
+    didThrow = true
     t.same(error.message, expectedMessage)
   }
+
+  t.ok(didThrow, `Expected function to throw with message: '${expectedMessage}'`)
 }
