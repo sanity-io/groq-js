987b9f7f0010789ded70aa537f345ec886f33258
Sindre Gulseth <sgulseth@gmail.com>
2024-02-28 19:30:01 +0100

    resolve: remove Optional type, use null-union

--- src/typeEvaluator/functions.ts
+++ src/typeEvaluator/functions.ts
@@ -3,19 +3,41 @@ import {walk} from './evaluateQueryType'
 import {Scope} from './scope'
 import {NullTypeNode, TypeNode} from './types'
 
+function unionWithoutNull(unionTypeNode: TypeNode): TypeNode {
+  if (unionTypeNode.type === 'union') {
+    return {
+      type: 'union',
+      of: unionTypeNode.of.filter((type) => type.type !== 'null'),
+    }
+  }
+  return unionTypeNode
+}
+
 export function handleFuncCallNode(node: FuncCallNode, scope: Scope): TypeNode {
   switch (`${node.namespace}.${node.name}`) {
     case 'global.defined': {
-      return {type: 'unknown'}
+      return {type: 'boolean'}
     }
     case 'global.coalesce': {
       if (node.args.length === 0) {
         return {type: 'null'} satisfies NullTypeNode
       }
+      const typeNodes: TypeNode[] = []
+      let canBeNull = true
+      for (const arg of node.args) {
+        const type = walk({node: arg, scope})
+        typeNodes.push(unionWithoutNull(type))
+        canBeNull =
+          type.type === 'null' || (type.type === 'union' && type.of.some((t) => t.type === 'null'))
+      }
+
+      if (canBeNull) {
+        typeNodes.push({type: 'null'} satisfies NullTypeNode)
+      }
 
       return {
         type: 'union',
-        of: node.args.map((arg) => walk({node: arg, scope})),
+        of: typeNodes,
       }
     }
     case 'pt.text': {
