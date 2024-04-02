80bb8cb202848e7e1a5282939d6d7f8ac04c89cd
Cody Olsen <81981+stipsan@users.noreply.github.com>
2024-04-02 12:16:17 +0200

    fix: remove typeEvaluator typesVersions export (#207)

--- src/typeEvaluator/index.ts
+++ src/typeEvaluator/index.ts
@@ -1,5 +1,24 @@
+export type {GroqFunction, GroqFunctionArg, GroqPipeFunction} from '../evaluator/functions'
+export type {Scope} from '../evaluator/scope'
+export type {Context, DereferenceFunction, Document, Executor} from '../evaluator/types'
+export * from '../nodeTypes'
+export type {
+  AnyStaticValue,
+  ArrayValue,
+  BooleanValue,
+  DateTimeValue,
+  GroqType,
+  NullValue,
+  NumberValue,
+  ObjectValue,
+  PathValue,
+  StaticValue,
+  StreamValue,
+  StringValue,
+  Value,
+} from '../values'
+export {DateTime, Path} from '../values'
 export {typeEvaluate} from './typeEvaluate'
-// @internal
 export {createReferenceTypeNode} from './typeHelpers'
 export type {
   ArrayTypeNode,
