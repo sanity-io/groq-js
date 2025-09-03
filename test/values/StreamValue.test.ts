ab6812d2f55c1b1160425c8d742b12e14ec91b0c
Dave Sewell <snocorp@gmail.com>
2025-09-03 14:45:10 -0400

    fix: Handle exceptions during StreamValue fetch

---
+++ test/values/StreamValue.test.ts
@@ -0,0 +1,27 @@
+import t from 'tap'
+import {StreamValue} from '../../src/values/StreamValue'
+import {fromJS} from '../../src/values'
+
+t.test('StreamValue', async (t) => {
+  t.test('handles exceptions during fetch', async (t) => {
+    const value = new StreamValue(async function* () {
+      throw new Error('test')
+    })
+
+    t.rejects(() => value.get(), 'blah')
+  })
+
+  t.test('returns yielded results', async (t) => {
+    const value = new StreamValue(async function* () {
+      yield fromJS(1)
+      yield fromJS(2)
+      yield fromJS(3)
+    })
+
+    const result = await value.get()
+    t.equal(result.length, 3)
+    t.equal(await result[0], 1)
+    t.equal(await result[1], 2)
+    t.equal(await result[2], 3)
+  })
+})
