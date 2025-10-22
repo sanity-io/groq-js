40f299cb05f8a5fa32b478d3e402e3b56c8730d8
Daniel Malmer <daniel.malmer@sanity.io>
2025-10-22 14:08:21 -0400

    feat(gdr): add unimplemented documents::incomingRefCount()

--- src/evaluator/functions/documents.ts
+++ src/evaluator/functions/documents.ts
@@ -5,4 +5,7 @@ const documents: FunctionSet = {}
 documents['get'] = constantExecutor(() => {
   throw new Error('not implemented')
 })
+documents['incomingRefCount'] = constantExecutor(() => {
+  throw new Error('not implemented')
+})
 export default documents
