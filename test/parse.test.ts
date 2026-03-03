5ba0cce8b13e83f0a363a2207dcbc209b960c852
Tor Bratsberg <tor@bratsberg.net>
2026-03-03 21:33:28 +0100

    fix: update semanticSimilarity test

--- test/parse.test.ts
+++ test/parse.test.ts
@@ -57,7 +57,7 @@ t.test('Basic parsing', async (t) => {
   })
 
   t.test('Can parse text::semanticSimilarity', async (t) => {
-    const query = `*[_type == "book"] | score(text::semanticSimilarity(embedding, text::embedding("monster")))[0..4] { _id, _score }`
+    const query = `*[_type == "book"] | score(text::semanticSimilarity("monster"))[0..4] { _id, _score }`
     const tree = parse(query)
     t.matchSnapshot(tree)
   })
